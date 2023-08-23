625aeef322f9dc3ede9be133731ae3b5afb1b234
Nina Merkulova <nina.magatina@gmail.com>
2023-08-23 23:02:17 +0300

    feat: add solution to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+    let nString = Math.abs(n).toString();
+    let result = "";
+
+    for( let i = nString.length - 1 ; i >= 0 ; i-- ) {
+      result += nString[i];
+    }
+
+    return Number(result);
 }
